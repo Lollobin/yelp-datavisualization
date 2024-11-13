ba5416efe7e50f4722b4ea3bd9b4c2b4471dbead
Benno Kossatz <e11909390@student.tuwien.ac.at>
2024-11-13 10:15:23 +0100

    refactor hexbin plot

--- Dashboard/hex_binning.py
+++ Dashboard/hex_binning.py
@@ -1,22 +1,21 @@
-import pandas as pd
 from bokeh.plotting import figure
-from bokeh.models import ColumnDataSource, CustomJS
-from bokeh.transform import linear_cmap
-from bokeh.util.hex import hexbin
+from bokeh.models import ColumnDataSource
 from pyproj import Transformer, CRS
 import xyzservices.providers as xyz
 
 
 def create_hexbin_plot(df):
-    # Load data and extract relevant columns and sample
-    df = df[["name", "latitude", "longitude"]].copy()
+    # Load data and extract relevant columns
+    #df = df[["name", "latitude", "longitude"]].copy()
 
     # Calculate web mercator coordinates
     in_proj = CRS.from_epsg(4326)  # WGS84
     out_proj = CRS.from_epsg(3857)  # Web Mercator
     transformer = Transformer.from_crs(in_proj, out_proj, always_xy=True)
     df["x"], df["y"] = zip(
-        *df.apply(lambda x: transformer.transform(x.longitude, x.latitude), axis=1)
+        *df.apply(
+            lambda row: transformer.transform(row["longitude"], row["latitude"]), axis=1
+        )
     )
     min_x, max_x = df["x"].min(), df["x"].max()
     min_y, max_y = df["y"].min(), df["y"].max()
@@ -24,7 +23,7 @@ def create_hexbin_plot(df):
     # Create plot
     source = ColumnDataSource(df)
     p = figure(
-        title="Philadelphia Restaurants",
+        title="Restaurants in Philadelphia",
         x_axis_type="mercator",
         y_axis_type="mercator",
         x_range=(min_x, max_x),
@@ -35,8 +34,8 @@ def create_hexbin_plot(df):
     )
     p.add_tile(xyz.CartoDB.Positron, retina=True)
     p.grid.visible = False
-    
-    
+    p.axis.visible = False
+
     p.hexbin(
         df["x"], df["y"], size=500, line_color=None, fill_alpha=0.5, syncable=False
     )
@@ -48,7 +47,7 @@ def create_hexbin_plot(df):
         radius=20,
         color="blue",
         alpha=0.1,
-        selection_color="red",
+        selection_color="firebrick",
     )
 
     return p, source
